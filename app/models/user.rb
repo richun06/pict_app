6fd9968518a688eb638856ad803db36974fb1614
rika kaminoyama <richun.06@gmail.com>
2022-10-09 19:32:08 +0900

    バリエーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :blogs
+
+  before_validation { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+end
